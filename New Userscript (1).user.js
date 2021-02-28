2cb1a0b95fc31d2fedbc2af48c4da0fe542ba777
MelvinWijkstra <79810270+MelvinWijkstra@users.noreply.github.com>
2021-03-01 00:51:50 +0400

    Add files via upload

---
+++ New Userscript (1).user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.amazon.nl/gp/buy/spc/handlers/display.html?hasWorkingJavascript=1
+// @match        https://www.amazon.de/gp/buy/spc/handlers/display.html?hasWorkingJavascript=1
+// @match        https://www.amazon.fr/gp/buy/spc/handlers/display.html?hasWorkingJavascript=1
+// @match        https://www.amazon.es/gp/buy/spc/handlers/display.html?hasWorkingJavascript=1
+// @match        https://www.amazon.co.uk/gp/buy/spc/handlers/display.html?hasWorkingJavascript=1
+// @match        https://www.amazon.it/gp/buy/spc/handlers/display.html?hasWorkingJavascript=1
+// @grant        none
+// ==/UserScript==
+
+(function() {
+location.reload();
+    document.querySelector("[name=placeYourOrder1]").click();
+})();
