2f22b0b4654770e5e1354d8eda8095c975b871de
MelvinWijkstra <79810270+MelvinWijkstra@users.noreply.github.com>
2021-03-01 01:15:31 +0400

    Add files via upload

---
+++ New Userscript (8).user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.amazon.de/*
+// @grant        none
+// @run-at       document-body
+// ==/UserScript==
+
+(function() {
+    document.querySelector("[name=placeYourOrder1]").click();
+})();
